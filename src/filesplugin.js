0c3bb3125475868f6ef5c4c608da4a592977ab31
Julien Veyssier <eneiluj@posteo.net>
2021-04-28 12:38:59 +0200

    clarify fileplugin action declaration
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/filesplugin.js
+++ src/filesplugin.js
@@ -30,6 +30,17 @@ import axios from '@nextcloud/axios'
 			'files.public',
 		],
 
+		async getState(fileId) {
+			const url = generateUrl('/apps/approval/' + fileId + '/state')
+			try {
+				const response = await axios.get(url)
+				return response.data.state
+			} catch (error) {
+				console.error(error)
+				return states.NOTHING
+			}
+		},
+
 		attach(fileList) {
 			if (this.ignoreLists.indexOf(fileList.id) >= 0) {
 				return
@@ -38,52 +49,46 @@ import axios from '@nextcloud/axios'
 			const approvalInfoView = new ApprovalInfoView()
 			fileList.registerDetailView(approvalInfoView)
 			OCA.Approval.View = approvalInfoView
+
+			fileList.fileActions.registerAction({
+				name: 'ApprovalStateInline',
+				render: async(actionSpec, isDefault, context) => {
+					const fileId = context.$file[0].dataset.id
+					const state = await this.getState(fileId)
+					if (state !== states.NOTHING) {
+						const actionLink = document.createElement('span')
+						actionLink.classList.add('approval-inline-state')
+						if (state === states.APPROVED) {
+							actionLink.classList.add('icon-approved')
+							actionLink.classList.add('icon-checkmark-white')
+						} else if (state === states.REJECTED) {
+							actionLink.classList.add('icon-rejected')
+							actionLink.classList.add('icon-close-white')
+						} else if (state === states.PENDING) {
+							actionLink.classList.add('icon-pending')
+							actionLink.classList.add('icon-checkmark-white')
+						} else if (state === states.APPROVABLE) {
+							actionLink.classList.add('icon-approvable')
+							actionLink.classList.add('icon-checkmark-white')
+							const tw = context.$file.find('a.name>.thumbnail-wrapper')
+							if (tw.length > 0) {
+								tw[0].setAttribute('title', t('approval', 'Pending approval, you are authorized to approve.'))
+							}
+						}
+						context.$file.find('a.name>.thumbnail-wrapper').append(actionLink)
+						return actionLink
+					}
+					return null
+				},
+				mime: 'all',
+				order: -140,
+				type: OCA.Files.FileActions.TYPE_INLINE,
+				permissions: OC.PERMISSION_READ,
+				actionHandler: null,
+			})
 		},
 	}
 
 })()
 
 OC.Plugins.register('OCA.Files.FileList', OCA.Approval.FilesPlugin)
-
-OCA.Files.fileActions.registerAction({
-	name: 'ApprovalStateInline',
-	async render(actionSpec, isDefault, context) {
-		const fileId = context.$file[0].dataset.id
-		const url = generateUrl('/apps/approval/' + fileId + '/state')
-		try {
-			const response = await axios.get(url)
-			const state = response.data.state
-			if (state !== states.NOTHING) {
-				const actionLink = document.createElement('span')
-				actionLink.classList.add('approval-inline-state')
-				if (state === states.APPROVED) {
-					actionLink.classList.add('icon-approved')
-					actionLink.classList.add('icon-checkmark-white')
-				} else if (state === states.REJECTED) {
-					actionLink.classList.add('icon-rejected')
-					actionLink.classList.add('icon-close-white')
-				} else if (state === states.PENDING) {
-					actionLink.classList.add('icon-pending')
-					actionLink.classList.add('icon-checkmark-white')
-				} else if (state === states.APPROVABLE) {
-					actionLink.classList.add('icon-approvable')
-					actionLink.classList.add('icon-checkmark-white')
-					const tw = context.$file.find('a.name>.thumbnail-wrapper')
-					if (tw.length > 0) {
-						tw[0].setAttribute('title', t('approval', 'Pending approval, you are authorized to approve.'))
-					}
-				}
-				context.$file.find('a.name>.thumbnail-wrapper').append(actionLink)
-				return actionLink
-			}
-		} catch (error) {
-			console.error(error)
-		}
-		return null
-	},
-	mime: 'all',
-	order: -140,
-	type: OCA.Files.FileActions.TYPE_INLINE,
-	permissions: OC.PERMISSION_READ,
-	actionHandler: null,
-})
