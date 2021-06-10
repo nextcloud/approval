6a076de48c7e945ade149f138087f85152058c68
Julien Veyssier <eneiluj@posteo.net>
2021-06-10 14:57:34 +0200

    beginning to implement tests
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nextcloud - approval
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Julien Veyssier <eneiluj@posteo.net>
+ * @copyright Julien Veyssier 2021
+ */
+
+require_once __DIR__.'/../../../lib/base.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+\OC_App::loadApp('approval');
